cf34add1136bb5da317334c0338ea2c51e6ac224
Kevin Hu <khu@liveperson.com>
2020-06-26 20:08:02 -0400

    added dev vs prod code

--- apis/utils/google-client.js
+++ apis/utils/google-client.js
@@ -1,22 +1,42 @@
 const { google } = require("googleapis");
 let { GoogleSpreadsheet } = require("google-spreadsheet");
-const creds = require("../../credentials");
-const {
-  client_secret,
-  client_id,
-  client_email,
-  private_key,
-  redirect_uris
-} = require("../../gs-credentials").installed;
+let client_secret;
+let client_id;
+let redirect_uri;
+if(process.env.ENV == "dev") {
+  client_secret = process.env.client_secret
+  client_id = process.env.client_id2
+  redirect_uri = process.env.redirect_uri
+} else if(process.env.ENV == "prod") {
+  const c = require('../../gs-credentials');
+  client_secret = c.client_secret
+  client_id = c.client_id
+  redirect_uri = c.redirect_uri[0]
+}
+
+const credsFromEnv = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url
+};
+const creds = process.env.ENV == "dev" ? require("../../credentials") : credsFromEnv;
 
 let getGoogleClient = async () => {
+  console.log(process.env.ENV)
   var doc = new GoogleSpreadsheet();
 
   await doc.useServiceAccountAuth(creds);
   const auth = new google.auth.OAuth2(
     client_id,
     client_secret,
-    redirect_uris[0]
+    redirect_uri
   );
   auth.setCredentials(doc.jwtClient.credentials);
   return auth;
